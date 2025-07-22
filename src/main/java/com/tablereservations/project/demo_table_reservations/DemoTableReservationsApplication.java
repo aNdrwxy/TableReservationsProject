1c6e4b9098e09d08107a38845cc873b6f3360d3f
andrwxy <andrewparedesbravoxd@gmail.com>
2025-07-22 15:43:55 -0500

    Proyecto Reserva de mesas (first commit)

---
+++ src/main/java/com/tablereservations/project/demo_table_reservations/DemoTableReservationsApplication.java
@@ -0,0 +1,13 @@
+package com.tablereservations.project.demo_table_reservations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoTableReservationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoTableReservationsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tablereservations/project/demo_table_reservations/DemoTableReservationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tablereservations.project.demo_table_reservations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoTableReservationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
